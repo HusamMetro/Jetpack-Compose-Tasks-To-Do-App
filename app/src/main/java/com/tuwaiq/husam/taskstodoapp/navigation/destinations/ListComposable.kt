803d0937a2b18bba5885fa80d20377fd17cce143
husam5009 <husam5009@hotmail.com>
2021-12-26 04:29:06 +0300

    version 1.17 :  1- Modified the Card layout for Expanded and non-expanded  2- Added the start Animation for Challenges List  3- Added Slide in animation and fadeIn to Task Screen

--- app/src/main/java/com/tuwaiq/husam/taskstodoapp/navigation/destinations/ListComposable.kt
+++ app/src/main/java/com/tuwaiq/husam/taskstodoapp/navigation/destinations/ListComposable.kt
@@ -1,19 +1,26 @@
 package com.tuwaiq.husam.taskstodoapp.navigation.destinations
 
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.navigation.*
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
 import com.google.accompanist.navigation.animation.composable
 import com.tuwaiq.husam.taskstodoapp.ui.screens.list.ListScreen
 import com.tuwaiq.husam.taskstodoapp.ui.viewmodels.SharedViewModel
 import com.tuwaiq.husam.taskstodoapp.util.Action
 import com.tuwaiq.husam.taskstodoapp.util.Constants.LIST_ARGUMENT_KEY
 import com.tuwaiq.husam.taskstodoapp.util.Constants.LIST_SCREEN
+import com.tuwaiq.husam.taskstodoapp.util.Constants.TASK_SCREEN
 import com.tuwaiq.husam.taskstodoapp.util.toAction
 
 @ExperimentalAnimationApi
@@ -27,7 +34,28 @@ fun NavGraphBuilder.listComposable(
         route = LIST_SCREEN,
         arguments = listOf(navArgument(LIST_ARGUMENT_KEY) {
             type = NavType.StringType
-        })
+        }),
+        /*exitTransition = { _,_ ->
+            fadeOut(animationSpec = tween(durationMillis = 300))},
+        enterTransition = { _,_ ->
+            fadeIn(animationSpec = tween(durationMillis = 300))
+        },*/
+        /*exitTransition = { initial, target ->
+            if (target.destination.navigatorName == TASK_SCREEN ) {
+                slideOutHorizontally(
+                    targetOffsetX = { 300 },
+                    animationSpec = tween(durationMillis = 300, easing = FastOutSlowInEasing),
+                ) + fadeOut(animationSpec = tween(durationMillis = 300))
+            }else{
+                fadeOut(animationSpec = tween(durationMillis = 300))
+            }
+        },
+        popEnterTransition = { _, _ ->
+            slideInHorizontally(
+                initialOffsetX = { -it },
+                animationSpec = tween(durationMillis = 300, easing = FastOutSlowInEasing),
+            )*//*+fadeIn(animationSpec = tween(durationMillis = 300))*//*
+        }*/
     ) { navBackStackEntry ->
         val action = navBackStackEntry.arguments?.getString(LIST_ARGUMENT_KEY).toAction()
 
